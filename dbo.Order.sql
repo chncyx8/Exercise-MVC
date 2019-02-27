a2e1f72fe3224bccec4d45f16a2daf7812636263
chncyx8 <42812465+chncyx8@users.noreply.github.com>
2019-02-26 21:36:06 -0500

    Add files via upload

---
+++ dbo.Order.sql
@@ -0,0 +1,21 @@
+﻿USE [Order_Product]
+GO
+
+/****** Object: Table [dbo].[Order] Script Date: 2/26/2019 9:33:14 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Order] (
+    [Order_id]   INT NOT NULL,
+    [Product_id] INT NOT NULL,
+    [User_id] INT NULL, 
+    [User_FisrtName] VARCHAR(50) NULL, 
+    [User_LastName] VARCHAR(50) NULL, 
+    PRIMARY KEY CLUSTERED ([Order_id] ASC), 
+    CONSTRAINT [FK_Product_id] FOREIGN KEY ([Product_id]) REFERENCES [Product]([Product_id])
+);
+
+
---
+++ dbo.Product.sql
@@ -0,0 +1,19 @@
+﻿USE [Order_Product]
+GO
+
+/****** Object: Table [dbo].[Product] Script Date: 2/26/2019 9:34:10 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Product] (
+    [Product_id]   INT          NOT NULL,
+    [Product_name] VARCHAR (50) NULL,
+    [Price]        FLOAT (53)   NULL,
+    PRIMARY KEY CLUSTERED ([Product_id] ASC)
+);
+
+
+
